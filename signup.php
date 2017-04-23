f9281c3235a217807c7c769f8c661b97c82c2de5
Ezra-Shimon Rosenfeld <srosenfeld613@gmail.com>
2017-04-23 00:44:00 -0400

    Added birthday and gender to signup page and fixed submit button so that it actually submits form data.

--- signup.php
+++ signup.php
@@ -14,24 +14,36 @@
 
             <br />
 
-            <form>
+            <form action="createMatches.php">
               <div class="form-group">
                 <div id="formGroupExampleInput1">
                   <div class="btn-group" data-toggle="buttons">
                     <label class="btn btn-warning" style="pointer-events: none;">
-                      <input type="radio" name="options" id="option1" autocomplete="off"> Are You Dating?
+                      <input type="radio" name="options" id="option1" autocomplete="off" required /> Are You Dating?
                     </label>
                     <label class="btn btn-primary active">
-                      <input type="radio" name="options" id="option2" autocomplete="on"> Yes
+                      <input type="radio" name="options" id="option2" autocomplete="on" /> Yes
                     </label>
                     <label class="btn btn-primary">
-                      <input type="radio" name="options" id="option3" autocomplete="off"> No
+                      <input type="radio" name="options" id="option3" autocomplete="off" /> No
+                    </label>
+
+                    <label>
+                      Birthday: <input type="date" name="birthday" required />
+                    </label>
+
+                    Gender:
+                    <label>
+                      <input type="radio" name="gender" value="guy" required /> Guy
+                    </label>
+                    <label>
+                      <input type="radio" name="gender" value="girl" /> Girl
                     </label>
                   </div>
                 </div>
                 <div id="formGroupExampleInput2">
                   <h3><small>Signup and join the revolution!</small></h3>
-                  <a class="btn btn-lg btn-success" href="createMatches.php">Get Started</a>
+                  <input type=submit class="btn btn-lg btn-success" value="Get Started" />
                 </div>
               </div>
             </form>
