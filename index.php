025b51508019047d62a51940838948b8c3a2a923
alex2binny <binnylewis@gmail.com>
2017-04-23 05:17:21 -0400

    adding facebook button. callbacks not created yet

--- index.php
+++ index.php
@@ -3,6 +3,49 @@
   <body>
 
 
+    <script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '1781961102019556',
+        xfbml      : true,
+        version    : 'v2.9'
+      });
+      FB.AppEvents.logPageView();
+      FB.getLoginStatus(function(response) {
+
+        if (response.status === 'connected') {
+          // the user is logged in and has authenticated your
+          // app, and response.authResponse supplies
+          // the user's ID, a valid access token, a signed
+          // request, and the time the access token
+          // and signed request each expire
+          var uid = response.authResponse.userID;
+          var accessToken = response.authResponse.accessToken;
+        } else if (response.status === 'not_authorized') {
+          // the user is logged in to Facebook,
+          // but has not authenticated your app
+        } else {
+          // the user isn't logged in to Facebook.
+        }
+      });
+    };
+
+
+
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+
+
+
+
+
     <div class="site-wrapper">
 
       <div class="site-wrapper-inner">
@@ -32,18 +75,22 @@
             <h1 class="cover-heading">You are the Shadchan</h1>
           </br>
             <p class="lead">Tinder is being turned on its keppeleh,</br>in this new twist where friends are connecting friends!</p>
-            <h3><small>Signup and join the revolution!</small></h3>
+            <h3><small>Log in with Facebook</small></h3>
+
+          <div class="fb-login-button" data-width="150" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
 
 
 
+  <!--
             <form>
               <div class="form-group">
                 <div id="formGroupExampleInput3">
                   <a class="btn btn-lg btn-primary" href="signup.php">Facebook</a>
-                  <a class="btn btn-lg btn-primary" href="createMatches.php">Login</a>
                 </div>
               </div>
             </form>
+  -->
+
 
           </div>
 
